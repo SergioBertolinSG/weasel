042da376388571596b653194e4dc53a3a2a01782
Morris Jobke <hey@morrisjobke.de>
2016-01-23 21:38:34 +0100

    Proper error handling/logging, sentry support and settings.php renamed

--- app/dependencies.php
+++ app/dependencies.php
@@ -4,6 +4,8 @@ use Interop\Container\ContainerInterface;
 use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Monolog\Handler\StreamHandler;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 // DIC configuration
 
@@ -15,7 +17,45 @@ $container['logger'] = function (ContainerInterface $c) {
     $logger = new Logger($settings['logger']['name']);
     $logger->pushProcessor(new UidProcessor());
     $logger->pushHandler(new StreamHandler($settings['logger']['path'], Logger::DEBUG));
+
+    $sentryDSN = $settings->get('sentry');
+    if(!empty($sentryDSN)) {
+        $sentryHandler = new \Monolog\Handler\RavenHandler(new Raven_Client($sentryDSN), Logger::WARNING);
+        $sentryHandler->setFormatter(new Monolog\Formatter\LineFormatter("%message%\n"));
+        $logger->pushHandler($sentryHandler);
+    }
+
     return $logger;
+};
+
+$container['errorHandler'] = function (ContainerInterface $c) {
+    return function (RequestInterface $request, ResponseInterface $response, \Exception $exception) use ($c) {
+
+        // Standard exception data
+        $error = [
+            'code' => $exception->getCode(),
+            'message' => $exception->getMessage(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => $exception->getTrace(),
+            'tracestring' => $exception->getTraceAsString()
+        ];
+
+        /** @var \Psr\Log\LoggerInterface[] $c */
+        $c['logger']->error($exception->getMessage(), $error);
+
+        // TODO
+        $production = false;
+
+        if($production) {
+            $error['message'] = 'There was an internal error';
+            unset($error['file'], $error['line'], $error['trace']);
+        }
+
+        $response->getBody()->write(json_encode($error));
+        return $response->withStatus(500)
+            ->withHeader('Content-Type', 'application/json');
+    };
 };
 
 // couchdb
